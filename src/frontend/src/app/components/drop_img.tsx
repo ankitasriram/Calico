ff6e29f76006ff7a551f05bdc3e80a4c7811e07f
nchshri <nhry89@gmail.com>
2025-08-02 16:26:56 -0400

    wokred on drag and drop function, got file explr to open on click?

--- src/frontend/src/app/components/drop_img.tsx
+++ src/frontend/src/app/components/drop_img.tsx
@@ -1,13 +1,63 @@
 import Image from "next/image";
+import { useCallback, useRef, useState } from "react";
 
-export default function Drop_Img() {
+export default function Drop_Img(onUploadComplete: (files: File[]) => void) {
+    const hiddenFileInput = useRef<HTMLInputElement>(null);
+    const [isDragging, setIsDragging] = useState(false);
+
+    const handleDragEnter = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(true);
+    }, []);
+
+    const handleDragLeave = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(false);
+    }, []);
+
+    const handleDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(true);
+    }, []);
+
+    const handleDrop = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setIsDragging(false);
+
+        const files = Array.from(e.dataTransfer.files);
+        if (files.length > 0) {
+            onUploadComplete(files);
+        }
+    }, [onUploadComplete]);
+
+    const handleClick = () => {
+        hiddenFileInput.current?.click();
+    }
+
+    const handleFileChange = (event: any) => {
+        const files = event.target.files;
+    };
 
     return (
-        <div
-            className="bg-white size-full py-[8vw] border-[0.3vw] border-dark-orange border-dotted rounded-[1vw] flex flex-col justify-center items-center text-dark-orange"
+        <object
+            className={`cursor-pointer dropzone ${isDragging ? 'dragging' : ''}`}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+            onClick={handleClick}
         >
-            <Image src="/upload_cloud.svg" alt="Upload" width={60} height={60}/>
-            drag & drop your image
-        </div>
+            <div
+                className="bg-white size-full py-[8vw] border-[0.3vw] border-dark-orange border-dotted rounded-[1vw] flex flex-col justify-center items-center text-dark-orange"
+            >
+                <Image src="/upload_cloud.svg" alt="Upload" width={60} height={60} />
+                drag & drop or click anywhere to upload image
+                <input type="file" onChange={handleFileChange} style={{ display: 'none' }} ref={hiddenFileInput}></input>
+            </div>
+        </object>
     );
 }
